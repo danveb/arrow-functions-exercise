390c9972067a0ca7e6c35f67aa530815b714f0e2
Daniel Bae <danibae91@icloud.com>
2021-02-14 00:05:18 -0800

    add Jasmine matcher

--- practice-arrow-functions.test.js
+++ practice-arrow-functions.test.js
@@ -14,5 +14,11 @@ describe('calculate taxes based on income', function() {
         // expect 275000 
         expect(calculateTaxes(275000)).toEqual(68750)
     })
+    // arrow function 
+    it('should calculate cut off for lower income-bracket', () => {
+        // expect 29000 to be less than X 
+        expect(calculateTaxes(29999)).toBeLessThan(4500)
+    })
+
 })
 
